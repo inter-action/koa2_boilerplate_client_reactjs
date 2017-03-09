ccf399a1230f826ebe82aca0535f1a27b42a932a
miao jing <243127395@qq.com>
2017-03-09 21:02:45 +0800

    add server side rendering capability

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  HashRouter,
+  Switch
+  // etc.
+} from "react-router-dom";
 
-const root = document.createElement("div");
-root.id = "app";
-document.body.appendChild(root);
+import { Home } from "./pages"
 
+if (!document.querySelector("#app")) {
+  const root = document.createElement("div");
+  root.id = "app";
+  document.body.appendChild(root);
+}
 
 
 // http://stackoverflow.com/questions/42095600/nested-routes-in-v4
 ReactDOM.render(
-    <div>home1</div>, document.querySelector("#app")
+  <Router>
+    <Route path="/" component={Home}></Route>
+  </Router>
+  , document.querySelector("#app")
 );
